a1e34423946e9fa9b28105cfd51aaf249c671785
Vinicius <viniciusgomes.33mes@gmail.com>
2024-11-12 13:05:32 -0300

    sql ajustes

--- banco.sql
+++ banco.sql
@@ -7,10 +7,26 @@ CREATE TABLE usuarios (
     nome VARCHAR(100)
 );
 
-INSERT INTO users (email, senha) VALUES ("teste", aes_encrypt("abc123","key"));
+CREATE TABLE IF NOT EXISTS barbearia (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    telefone VARCHAR(15) NOT NULL,
+    formas_pagamento VARCHAR(255),
+    quantidade_funcionarios ENUM('1 a 2 Funcionarios', '1 a 4 Funcionarios', '1 a 6 ou +') NOT NULL,
+    proprietario VARCHAR(100) NOT NULL,
+    dias_servico ENUM('segunda a sexta', 'todos os dias') NOT NULL,
+    localizacao VARCHAR(100) NOT NULL,
+    horario_funcionamento VARCHAR(20) NOT NULL
+) ENGINE=InnoDB;
 
-UPDATE users SET email = "teste@gmail.com" WHERE users.email LIKE '%teste@gmail.com%';
-
-SELECT * FROM Users;
-
-DELETE FROM users WHERE users.email LIKE '%teste@gmail.com%';
+CREATE TABLE IF NOT EXISTS agendamento (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_barbearia INT NOT NULL,
+    nome_cliente VARCHAR(100) NOT NULL,
+    email_cliente VARCHAR(100) NOT NULL,
+    telefone_cliente VARCHAR(15) NOT NULL,
+    forma_pagamento ENUM('Cartão de Crédito', 'Boleto Bancário', 'Pix') NOT NULL,
+    data_horario DATETIME NOT NULL,
+    FOREIGN KEY (id_barbearia) REFERENCES barbearia(id) ON DELETE CASCADE
+) ENGINE=InnoDB;
